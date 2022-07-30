ea3785e0154e410918539abe749557ddd7dda6fd
Luke O'Brien <lukeobrien02@gmail.com>
2022-07-30 16:36:57 +0100

    [FA-9]  secure route set up

---
+++ middleware/secure_route.py
@@ -0,0 +1,44 @@
+from http import HTTPStatus
+
+import jwt
+from functools import wraps
+from flask import request, g
+from models.user import UserModel
+from config.environment import secret 
+
+
+def secure_route(route_function):
+
+    @wraps(route_function)
+    def decorated_function(*args, **kwargs):
+
+        raw_token = request.headers.get("Authorization")
+
+        if not raw_token:
+            return {"message": "Unauthorized"}, HTTPStatus.Unauthorized
+
+        clean_token = raw_token.replace("Bearer ", "")
+        
+        try:
+            payload = jwt.decode(clean_token, secret, "HS256")
+            #remember to import the secret from config > environment
+
+            user_id = payload["sub"]
+
+            user = UserModel.query.get(user_id)
+
+            if not user: 
+                return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+
+            g.current_user = user
+
+        except jwt.ExpiredSignatureError: 
+            return {"message": "Token has expired"}, HTTPStatus.UNAUTHORIZED
+
+        except Exception as e: 
+            return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+
+
+        return route_function(*args, **kwargs)
+
+    return decorated_function
