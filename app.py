d010f053ffaa4197ceed9837d8c0dd018bdcfd58
Luke O'Brien <lukeobrien02@gmail.com>
2022-07-30 16:25:01 +0100

    [FA-6] app.py file set up

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-# * Hello world flask app.
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
-# ? Instantiate flask
-# ? __name__ is going to be a different value depending on
-# ? where you run flask from. If you run this directly,
-# ? it will be '__main__'
+from config.environment import db_URI
+
 app = Flask(__name__)
+# ! Configuring it with flask
+app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
+# ! Removes a warning for an unused part of the library
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# ? Make a very basic route to talk to...
-# * This @ syntax is a 'Decorator'. This decorator tell us
-# * Which route our function belong to (our path for this route)
-@app.route("/hello")
-def home():
-    return { "hello": "world" }
+db = SQLAlchemy(app)
+
+# ! Instatiates Marshmallow framework
+ma = Marshmallow(app)
+
+bcrypt = Bcrypt(app)
+
+# from controllers import coffees, preparation, users
+
+# app.register_blueprint(coffees.router, url_prefix="/api")
+# app.register_blueprint(preparation.router, url_prefix="/api")
+# app.register_blueprint(users.router, url_prefix="/api")
+
+
+
+
+
+
+
