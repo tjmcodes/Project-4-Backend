04e05e2b8f64ad04c54f5c39cdf90efe3c355825
Luke O'Brien <lukeobrien02@gmail.com>
2022-07-30 18:34:59 +0100

    [FA-51] venuedata

---
+++ controllers/venues.py
@@ -0,0 +1,56 @@
+from http import HTTPStatus
+from marshmallow.exceptions import ValidationError
+from flask import Blueprint, request
+from models.venue import VenueModel
+from serialisers.venue import VenueSchema
+
+
+venue_schema = VenueSchema()
+
+router = Blueprint("users", __name__)
+
+@router.route('/venue-signup', methods=["POST"])
+def register():
+    try:
+        user_dictionary = request.json
+        user = venue_schema.load(user_dictionary)
+        user.save()
+        return venue_schema.jsonify(user)
+    except ValidationError as e:
+        return {"errors": e.messages, "messages": "Something went wrong"}
+    except Exception as e:
+        return { "messages": "Something went wrong" }
+
+
+@router.route('/venue-login', methods=["POST"])
+def login():
+    try:
+        credentials_dictionary = request.json
+        user = VenueModel.query.filter_by(email=credentials_dictionary["email"]).first()
+        if not user:
+            return {"message": "No Registered user with this email, please re-enter email"}
+        if not user.validate_password(credentials_dictionary["password"]):
+            return {"message": "You are not authorized"}, HTTPStatus.UNAUTHORIZED
+        token = user.generate_token()
+        return {"token": token, "message": "Welcome back!"}
+    except Exception as e:
+        return {"messages": "Something went wrong" }
+        print (e)
+
+
+@router.route("/allvenues", methods=["GET"])
+def get_teas():
+    teas = VenueModel.query.all()
+
+    return venue_schema.jsonify(teas, many=True), HTTPStatus.OK
+
+
+@router.route("/allvenues/<int:venue_id>", methods=["GET"])
+def get_single_tea(tea_id):
+
+    tea = VenueModel.query.get(tea_id)
+
+    if not tea:
+        return {"message": "venue not found"}, HTTPStatus.NOT_FOUND
+
+    return venue_schema.jsonify(tea), HTTPStatus.OK
---
+++ models/venuedata.py
@@ -0,0 +1,25 @@
+from models.venue import VenueModel
+
+
+
+teas_list = [
+    VenueModel(
+        username="Windmill", 
+        password="Windmill1", 
+        email="Windmill@Windmill.com", 
+        venueImage="https://upload.wikimedia.org/wikipedia/commons/6/6a/Windmill%2C_Brixton_Hill%2C_SW2_%283151353450%29.jpg",
+        address="22 Blenheim Gardens, Brixton Hill, London SW2 5BZ", 
+        budget=100, 
+        websiteUrl="https://www.windmillbrixton.co.uk/", 
+        videoUrl="", 
+        optionUrl="", 
+        cardimage="",
+        galleryImage1="", 
+        galleryImage2="", 
+        galleryImage3="", 
+        description="", 
+        socialMediaUrl1="", 
+        socialMediaUrl2="", 
+        socialMediaUrl3=""),
+
+]
