b16418536ef90c4f1863d1eaea0642a5f4d05c2f
Luke O'Brien <lukeobrien02@gmail.com>
2022-08-04 17:29:57 +0100

    [FA-56] venue types table added

--- controllers/types.py
+++ controllers/types.py
@@ -6,6 +6,8 @@ from marshmallow.exceptions import ValidationError
 from serialisers.venue import VenueSchema
 from serialisers.venue_types import TypeSchema
 from models.types import TypeModel
+from models.venue import VenueModel
+from middleware.venue_secure_route import venue_secure_route
 
 
 Venue_schema = VenueSchema()
@@ -38,3 +40,22 @@ def remove_genre(type_id):
     type.remove()
 
     return '', HTTPStatus.NO_CONTENT
+
+
+
+@router.route("/venues/<int:venue_id>/type/<int:type_id>", methods=["POST"])
+@venue_secure_route
+def create_tea_note(venue_id, type_id):
+
+    venue = VenueModel.query.get(venue_id)
+
+    type = TypeModel.query.get(type_id)
+
+    if not type or not venue:
+        return {"message": "Item not found"}, HTTPStatus.NOT_FOUND
+
+    venue.notes.append(type)
+
+    venue.save()
+
+    return Venue_schema.jsonify(venue), HTTPStatus.OK
