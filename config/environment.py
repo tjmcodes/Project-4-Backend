63bb61fc7ff7f66f23fa4ee8e7917322ef24615c
Teresa Morini <tjmcodes@gmail.com>
2022-08-13 10:24:23 +0100

    IP address added for remote set up

--- config/environment.py
+++ config/environment.py
@@ -1,3 +1,5 @@
-db_URI = "postgresql://localhost:5432/VenuesandArtists_db"
+db_URI = "postgresql://127.0.0.1:5432/VenuesandArtists_db"
+# db_URI = "postgresql://localhost:5432/VenuesandArtists_db"
 
 secret = "AlienToughPreparatoryPlacid2"
+
